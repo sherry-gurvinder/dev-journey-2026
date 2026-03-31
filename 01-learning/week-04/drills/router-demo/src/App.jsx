3802991f894753712dab8995fae1dd59b32edd17
sherry-gurvinder <77814528+sherry-gurvinder@users.noreply.github.com>
2026-03-31 14:00:35 +0000

    Week 4 Day 1: BrowserRouter, Routes, Route, Link working

--- 01-learning/week-04/drills/router-demo/src/App.jsx
+++ 01-learning/week-04/drills/router-demo/src/App.jsx
@@ -1,6 +1,21 @@
+import { Routes,Route,Link } from "react-router-dom";
+import CreatePost from './pages/CreatePost'
+import Dashboard from './pages/Dashboard'
+import UserList from './pages/UserList'
 function App() {
   return (
     <div>
+      <nav>
+      <Link to="/">Dashboard</Link>
+      <Link to="/createpost">Create Post</Link>
+      <Link to="/userlist">User List</Link>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}></Route>
+        <Route path="createpost" element={<CreatePost/>}></Route>
+        <Route path="userlist" element={<UserList/>}></Route>
+      </Routes>
       <h1>Week 4 - React Router</h1>
     </div>
   );
---
+++ 01-learning/week-04/drills/router-demo/src/pages/UserList.jsx
@@ -0,0 +1,7 @@
+const UserList = () =>
+{
+    return<>
+    <h1> User List Page</h1>
+    </>
+}
+export default UserList;
