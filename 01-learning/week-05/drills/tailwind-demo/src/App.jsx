6f5c04a73a2abd7e390d8f69ced366846d68129d
sherry-gurvinder <77814528+sherry-gurvinder@users.noreply.github.com>
2026-04-03 16:03:35 +0000

    Week 5 Day 2: Navbar, card grid, responsive layout

--- 01-learning/week-05/drills/tailwind-demo/src/App.jsx
+++ 01-learning/week-05/drills/tailwind-demo/src/App.jsx
@@ -1,6 +1,18 @@
 const App = () =>
 {
   return(   
+      <div>
+        <nav className="bg-gray-900 text-white px-6 py-4">
+         <div className="flex justify-between items-center">
+           <div >Logo</div>
+         <div className="flex gap-6">
+          <a href=""> Link 1</a>
+          <a href=""> Link 1</a>
+          <a href=""> Link 1</a>
+         </div>
+          </div>
+        </nav>
+       
     
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
            <div className="bg-white w-96 p-6 rounded-4xl shadow-lg">
@@ -13,7 +25,23 @@ const App = () =>
                 View Profile
             </button>
           </div>
-        </div>      
+        </div>
+        <div className="bg-white-900 ">
+            <div className="grid justify-center p-6">
+              <h1 className="text-2xl">Our Services</h1>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-16 px-6">
+              <div className="bg-gray-100 rounded-lg shadow-md p-6">
+                <h1>Title</h1>
+                <h3>Descriptions</h3>
+              </div>
+              <div className="bg-gray-100 rounded-lg shadow-md p-6"><h1>Title</h1>
+                <h3>Descriptions</h3></div>
+              <div className="bg-gray-100 rounded-lg shadow-md p-6"> <h1>Title</h1>
+                <h3>Descriptions</h3></div>
+            </div>
+        </div>
+      </div>      
   )
 }
 export default App;
