5193071160c67f445ea8601490acad0461a48ab2
sherry Gurvinder <sgurvinder0594@gmail.com>
2026-04-04 21:12:26 -0400

    Week 5 Day 4: Form validation, error handling, success state

--- 01-learning/week-05/drills/tailwind-demo/src/App.jsx
+++ 01-learning/week-05/drills/tailwind-demo/src/App.jsx
@@ -1,5 +1,6 @@
 import Badge from './components/Badge'
 import Button from './components/Button';
+import Login from './pages/Login';
 const App = () =>
 {
   return(   
@@ -9,37 +10,16 @@ const App = () =>
          <div className="flex justify-between items-center">
            <div >Logo</div>
          <div className="flex gap-6">
-          <a href=""> Link 1</a>
+         <a href=""> Link 1</a>
           <a href=""> Link 1</a>
           <a href=""> Link 1</a>
          </div>
           </div>
         </nav>
        
-    
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-           <div className="bg-white w-96 p-6 rounded-4xl shadow-lg">
-            <p className="font-bold text-xl text-gray-900">Name:</p>
-            <p className="text-sm text-gray-500">Job Title:</p>
-            <hr/>
-            <p className="text-sm text-gray-500 uppercase">Email:</p>
-            <p className="text-base text-gray-900">Value</p>
-             <div className="flex gap-2 mt-2">
-              <Badge text="Active" color="green" />
-              <Badge text="Pending" color="blue" />
-              <Badge text="Inactive" color="red" />
-            </div>
-            <div className="flex gap-2">
-              <Button variant="primary">Save</Button>
-              <Button variant="secondary">Cancel</Button>
-              <Button variant="danger">Delete</Button>
-            </div>
-            <button className="bg-blue-600 w-full py-2 rounded-lg text-white font-medium mt-4 hover:bg-blue-700 transition-colors duration-200">
-                View Profile
-            </button>
-            
-          </div>
-        </div>
+        <Login/>
+       
+
         <div className="bg-white-900 ">
             <div className="grid justify-center p-6">
               <h1 className="text-2xl">Our Services</h1>
---
+++ 01-learning/week-05/drills/tailwind-demo/src/pages/Login.jsx
@@ -0,0 +1,84 @@
+import Button from "../components/Button";
+import { useState } from "react";
+const Login = () =>
+{   const [error, setError] = useState("");
+    const [Email,setEmail] = useState("");
+    const [Password,setPassword] = useState("");
+    const [LoginSuccess,setLoginSuccess] =useState("");
+    const emailChange = (e) =>
+    {
+        setEmail(e.target.value)
+        setError("");
+        setLoginSuccess(false);
+
+    }
+    const passwordChange = (e) =>
+
+        {
+            setPassword(e.target.value)
+            setError("");
+            setLoginSuccess(false);
+
+        }
+    const handleSubmit =(e) =>
+    {   
+        setLoginSuccess(false);
+        console.log("handle click")
+        e.preventDefault();
+        if(!Email.includes("@"))
+        {
+            console.log("error @ is missing ")
+            setError("Invalid Email")
+            return;
+        }
+        else if(Password.length<=5)
+        {
+            console.log("password is less than 5")
+            setError("Password should be longer than 5 characters")
+            return
+        }
+        else{
+            console.log("no error")
+            setLoginSuccess(true);
+        }
+    }
+    return(
+        <div className="grid">
+            <form className="grid bg-gray-200 p-6 m-6 justify-center item-center">
+                <div className="grid justify-center">
+                {error && <p className="text-red-500">{error}</p>}
+                {LoginSuccess && <p className="text-green-500">Login successful</p>}
+                </div>
+                <div className="grid gap-4 grid-cols-2">
+                   
+
+                    <label>Email</label>
+                    <input 
+                    className="border-2 border-gray-300 rounded-lg
+                    px-2 py-2 m-2
+                    focus:border-blue-500 focus:ring-2 focus:ring-blue-200
+                    transition-colors" 
+                    type="text" placeholder="Email" value={Email} onChange={emailChange}/>
+                </div>
+                <div className="grid gap-4 grid-cols-2">
+                    <label>Password</label>
+                    <input className="border-2 border-gray-300 rounded-lg
+                    px-2 py-2 m-2
+                    focus:border-blue-500 focus:ring-2 focus:ring-blue-200
+                    transition-colors" 
+                    type="password" placeholder="Password" value={Password} onChange={passwordChange}/>
+                </div>
+                <div className="grid grid-cols-2">
+                    <input type="checkbox" placeholder="Password" className="justify-right"/>
+                    <label>Remember me</label>
+                </div>
+                <div className="grid justify-center">
+                    <button onClick={handleSubmit} className="p-4 rounded-full bg-orange-500 hover:bg-blue-400 border-4 border-blue-500 hover:border-blue-500 hover:text-white-500">Sign In </button>
+                </div>
+            </form>
+            
+            
+        </div>
+    )
+}
+export default Login;
