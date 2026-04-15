3d0956c52c7ef566e8cf825e65b1808d59ffe980
sherry-gurvinder <77814528+sherry-gurvinder@users.noreply.github.com>
2026-04-15 17:58:49 +0000

    Week 6 Wednesday: Drill 3 - useReducer context structure (reducer + provider)

---
+++ 01-learning/week-06/drills/src/drills/drill-03-reducer/contexts/CounterContext.jsx
@@ -0,0 +1,27 @@
+import { createContext , useReducer} from "react"
+
+const CounterContext = createContext();
+ const counterReducer = (state,action) =>
+    {
+        if(action.type ==="INCREMENT")
+        {
+            return state + 1;
+        }
+        if(action.type==="DECREMENT")
+        {
+            return state - 1;
+        }
+         if(action.type==="RESET")
+        {
+            return 0;
+        }
+    }
+const CounterProvider = ({children}) =>
+{
+   const [count,dispatch] = useReducer(counterReducer,0);
+    return(<CounterContext.Provider value = {[count,dispatch]}>
+        {children}
+    </CounterContext.Provider>)
+}
+
+export { CounterContext, CounterProvider };
